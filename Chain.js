88b21839fda248d93028aef9acaa3c29c9108efe
Yash-Vardhan2209 <72913044+Yash-Vardhan2209@users.noreply.github.com>
2020-12-08 16:06:47 +0530

    Add files via upload

---
+++ Chain.js
@@ -0,0 +1,18 @@
+class Chain {
+    constructor(bodyA, bodyB){
+var options= {
+    bodyA:bodyA,
+    bodyB:bodyB,
+    stiffness:0.05,
+    length:30
+}
+this.chain=Constraint.create(options);
+World.add(world,this.chain);
+    }
+display(){
+    var pointA=this.chain.bodyA.position;
+    var pointB=this.chain.bodyA.position;
+    strokeWeight(6);
+    line(pointA.x,pointA.y,pointB.x,pointB.y);
+}
+}
